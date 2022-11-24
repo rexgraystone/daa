17d6444e8fdd5e1dcbeb811b0a7aafbb3d419d9c
Raghav Nanjappan <r6nanjappan@gmail.com>
2022-11-24 08:32:04 +0530

    Update programs

--- 6_kruskalsAlgorithm.c
+++ 6_kruskalsAlgorithm.c
@@ -6,39 +6,40 @@
 # include <time.h>
 
 # define infinity 999
-
 typedef struct graph {
-    int v1, v2, cost;
-} gr;
+    int v1;
+    int v2;
+    int cost;
+}gr;
 
 gr g[20];
-int nodes, edges;
 
-void spanningTree();
+int nodes,edges;
 
 int main() {
     int k, startTime, endTime, totalTime;
+    void spanningTree();
     printf("\n\tGRAPH CREATION\n");
-    printf("Enter no of nodes\n");
+    printf("Enter the number of nodes: ");
     scanf("%d", &nodes);
-    printf("Enter no edges\n");
+    printf("Enter the number of edges: ");
     scanf("%d", &edges);
     for(k = 0; k < edges; k++) {
-        printf("Enter edge v1 to v2:");
+        printf("Enter the vertices 'v1' and 'v2' that connect edge '%d': ", k + 1);
         scanf("%d %d", &g[k].v1, &g[k].v2);
-        printf("Enter the cost:");
+        printf("Enter the cost for the edge '%d' that connects the vertices '%d' and '%d': ", k + 1, g[k].v1, g[k].v2);
         scanf("%d", &g[k].cost);
     }
     startTime = clock();
     spanningTree();
     endTime = clock();
     totalTime = endTime - startTime;
-    printf("\nStart Time = %d\n	End Time = %d\n	Total Time = %d\n", startTime, endTime, totalTime);
+    printf("\nStart Time = %d\nEnd Time = %d\nTotal Time = %d ms\n", startTime, endTime, totalTime);
     return 0;
 }
 
 int find(int v2, int parent[]) {
-    while(parent[v2]) {
+    while(parent[v2] != v2) {
         v2 = parent[v2];
     }
     return v2;
@@ -47,7 +48,7 @@ int find(int v2, int parent[]) {
 void unionFunc(int i, int j, int parent[10]) {
     if(i < j) {
         parent[j] = i;
-    }
+    } 
     else {
         parent[i] = j;
     }
@@ -70,12 +71,11 @@ void spanningTree() {
     int count, k, v1, v2, i, j, tree[10][10], pos, parent[10];
     int sum;
     count = 0;
-    k = 0; 
+    k = 0;
     sum = 0;
-    for(i = 0; i < nodes; i++) {
-        parent[i] = i;
-    }
-    while(count != (nodes - 1)) {
+    for(i = 0; i < nodes; i++)
+    parent[i] = i;
+    while(count != nodes - 1) {
         pos = minimum(edges);
         if(pos == -1) {
             break;
@@ -92,19 +92,21 @@ void spanningTree() {
             sum += g[pos].cost;
             unionFunc(i, j, parent);
         }
-        g[pos].cost = infinity;
+        g[pos].cost=infinity;
     }
-    if(count == (nodes - 1)) {
+    if(count == nodes - 1) {
         printf("\n\tSPANNING TREE IS:\n");
         printf("\n");
-        for(i = 0; i < (nodes - 1); i++) {
-            printf("\n%d", tree[i][0]);
+        for(i = 0; i < nodes - 1; i++) {
+            printf("\n\t%d", tree[i][0]);
             printf("-");
             printf("%d\n", tree[i][1]);
         }
-        printf("\nCost of spanning tree is: %d", sum);
+        printf("\nThe cost of the spanning tree is: %d", sum);
     }
     else {
-        printf("No spanning tree");
+        printf("No spanning tree:");
     }
 }
+
+
